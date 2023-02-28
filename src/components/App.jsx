75f426a046be08eb4e5f785dbfa01e617ba95636
19Lida <lida19melnyk@gmail.com>
2023-02-28 18:45:59 +0200

    add methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,18 @@ export class App extends Component {
     ],
     filter: '',
   };
-
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   onHandleSubmit = data => {
     const name = data.name;
     const number = data.number;
